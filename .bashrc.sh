ddb537f88be01e6740810a5aa3d2d07fe756666e
JohnMrMoe <arenor.moe@gmail.com>
2020-11-20 18:42:44 +0100

    COPYMODE

---
+++ .bashrc.sh
@@ -0,0 +1,5 @@
+rm cd ../../behavior_packs/deepcraft_behavior_pack
+rm cd ../../resource_packs/deepcraft_resource_pack
+
+cp deepcraft_behavior_pack ../../behavior_packs/
+cp deepcraft_resource_pack ../../resource_packs/
